48ba6b7325a8d3d4fe6dfcf603622cda3c34a5f6
zimma08 <arthur.zimmermann@univille.br>
2024-09-04 23:46:01 -0300

    Delete POO-II-main/POO-II-main/JPA Api 1/main directory

--- POO-II-main/POO-II-main/JPA Api 1/main/java/br/edu/univille/br/spa/modelo/repositorio/PessoaRepositorio.java
+++
@@ -1,8 +0,0 @@
-package br.edu.univille.br.spa.modelo.repositorio;
-
-import br.edu.univille.br.spa.modelo.entidade.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepositorio
-        extends JpaRepository<Pessoa, Long> {
-}
--- POO-II-main/POO-II-main/JPA Api 1/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into pessoa(id,nome,CPF) values (1,'Tom Cruise','000.000.000-12')
-insert into pessoa(id,nome,CPF) values (2,'John Travolta','000.000.000-34')
